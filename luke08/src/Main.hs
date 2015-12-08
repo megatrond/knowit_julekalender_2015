cb4f44a4950b79b223e88ae6643edc08a0ecd9d8
Trond Sørås <trond@origin.no>
2015-12-08 17:03:16 +0100

    luke08

---
+++ luke08/src/Main.hs
@@ -0,0 +1,18 @@
+module Main where
+
+iSqrt :: Int -> Int
+iSqrt = floor . sqrt . fromIntegral
+
+isPrime :: Int -> Bool
+isPrime k = null [ x | x <- [2..iSqrt k], k `mod`x  == 0]
+
+iReverse :: Int -> Int
+iReverse i = read (reverse (show i))
+
+mirps :: Int -> Int
+mirps i = length [ x | x <- [0..i], (isPrime x && isPrime (iReverse x) && x /= (iReverse x) ) ]
+
+main :: IO ()
+main = do
+    print (mirps 1000)
+    
